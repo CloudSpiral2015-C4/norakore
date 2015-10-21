6212740c3fdc243058e8d90c048d67e825d9a678
kazuki-mo <moriya.kazuki.me0@is.naist.jp>
2015-10-22 01:38:47 +0900

    alpaca commit3

---
+++ src/jp/kobe_u/cspiral/alpaca/util/DBUtils.java
@@ -0,0 +1,42 @@
+package jp.kobe_u.cspiral.alpaca.util;
+
+import java.net.UnknownHostException;
+
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+
+/**
+ * Mongodbのコネクション管理クラス
+ * DB接続へのシングルトン管理を行う．
+ *
+ * @author shin
+ *
+ */
+public class DBUtils {
+	public static DBUtils instance = new DBUtils();
+
+	private DB db;
+
+	//private final String host = new String("");
+	private final String host = new String("192.168.10.2");
+	private final String dbName = new String("alpaca_c0");
+
+	private DBUtils() {
+		try {
+			Mongo m = new Mongo(host);
+			db = m.getDB(dbName);
+		} catch (UnknownHostException | MongoException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static DBUtils getInstance() {
+		return DBUtils.instance;
+	}
+
+	public DB getDb() {
+		return this.db;
+	}
+
+}
