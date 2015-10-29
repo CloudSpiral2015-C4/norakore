bd7ec9f318fc082bfff4d976a92c6f14ac69f11b
今江健悟 <kengo@imaekengo-no-MacBook-Air.local>
2015-10-29 15:33:44 +0900

    既存Webページのリンクを修正 + 見本アイコン作成

--- web/js/common.js
+++ web/js/common.js
@@ -9,24 +9,20 @@
  */
 function getUserInfoHeader(username) {
 	var header = document.getElementById('user-info-header');
-	header.innerHTML = '<div class="row text-center"><div class="col-xs-4"><a href="#"><div class="btn btn-custom" data-color="blue" font-size="1.2"><p>トップ</p></div></a></div><div class="col-xs-4"><p id="username">' + username + '</p></div><div class="col-xs-4"><a href="#"><div class="btn btn-custom" data-color="blue" font-size="1.1"><p>ログアウト</p></div></a></div></div>';
-	return;
+	var headerHTML = '';
+	headerHTML += '<div class="row text-center">';
+	headerHTML += '    <div class="col-xs-4">';
+	headerHTML += '        <div class="btn btn-custom" data-color="blue" font-size="1.2" onclick="location.href=\'index.html\'"><p>トップ</p></div>';
+	headerHTML += '    </div>';
+	headerHTML += '    <div class="col-xs-4">';
+	headerHTML += '        <p id="username">' + username + '</p>';
+	headerHTML += '    </div>';
+	headerHTML += '    <div class="col-xs-4">';
+	headerHTML += '        <div class="btn btn-custom" data-color="blue" font-size="1.1" onclick="location.href=\'#\'"><p>ログアウト</p></div>';
+	headerHTML += '    </div>';
+	headerHTML += '</div>';
+	header.innerHTML = headerHTML;
 }
-/*
-		'
-		<div class="row text-center">
-			<div class="col-xs-4">
-				<a href="#"><div class="btn btn-custom" data-color="blue" font-size="1.2"><p>トップ</p></div></a>
-			</div>
-			<div class="col-xs-4">
-				<p id="username">' + username + '</p>
-			</div>
-			<div class="col-xs-4">
-				<a href="#"><div class="btn btn-custom" data-color="blue" font-size="1.1"><p>ログアウト</p></div></a>
-			</div>
-		</div>
-		'
-*/
 
 
 
@@ -37,18 +33,16 @@ function getUserInfoHeader(username) {
  */
 function getMenuButtonFooter(message) {
 	var footer = document.getElementById('menu-button-footer');
-	footer.innerHTML = '<p id="footer-info-message">' + message +'</p><div id="footer"><div class="btn btn-custom" data-color="yellow" data-type="footer"><a href="#"><img src="images/top.png"></a></div><div class="btn btn-custom" data-color="yellow" data-type="footer"><a href="#"><img src="images/top.png"></a></div><div class="btn btn-custom" data-color="yellow" data-type="footer"><a href="#"><img src="images/top.png"></a></div><div class="btn btn-custom" data-color="yellow" data-type="footer"><a href="#"><img src="images/top.png"></a></div><div class="btn btn-custom" data-color="yellow" data-type="footer"><a href="#"><img src="images/top.png"></a></div></div>';
+	var footerHTML =  '';
+	footerHTML += '<p id="footer-info-message">' + message + '</p>';
+	footerHTML += '<div id="footer">';
+	footerHTML += '<div class="btn btn-custom" data-color="yellow" data-type="footer" onclick="location.href=\'search.html\'"><img src="images/search.png"></div>';
+	footerHTML += '<div class="btn btn-custom" data-color="yellow" data-type="footer" onclick="location.href=\'upload.html\'"><img src="images/upload.png"></div>';
+	footerHTML += '<div class="btn btn-custom" data-color="yellow" data-type="footer" onclick="location.href=\'my-nyavatars.html\'"><img src="images/my-nyavatars.png"></div>';
+	footerHTML += '<div class="btn btn-custom" data-color="yellow" data-type="footer" onclick="location.href=\'scanner.html\'"><img src="images/scanner.png"></div>';
+	footerHTML += '<div class="btn btn-custom" data-color="yellow" data-type="footer" onclick="location.href=\'shop.html\'"><img src="images/shop.png"></div>';
+	footerHTML += '</div>';
+	footer.innerHTML = footerHTML;
 	return;
 }
-/*
-	'
-	<p id="footer-info-message">' + message + ' </p>
-	<div id="footer">
-		<div class="btn btn-custom" data-color="yellow" data-type="footer"><a href="#"><img src="images/top.png"></a></div>
-		<div class="btn btn-custom" data-color="yellow" data-type="footer"><a href="#"><img src="images/top.png"></a></div>
-		<div class="btn btn-custom" data-color="yellow" data-type="footer"><a href="#"><img src="images/top.png"></a></div>
-		<div class="btn btn-custom" data-color="yellow" data-type="footer"><a href="#"><img src="images/top.png"></a></div>
-		<div class="btn btn-custom" data-color="yellow" data-type="footer"><a href="#"><img src="images/top.png"></a></div>
-	</div>
-	'
-*/
+
