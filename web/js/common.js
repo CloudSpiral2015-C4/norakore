c6796b7c1e31bc510b31ef7ae023c4ef77fccd54
今江健悟 <kengo@imaekengo-no-MacBook-Air.local>
2015-10-30 14:15:01 +0900

    アップロードページを作成しました

--- web/js/common.js
+++ web/js/common.js
@@ -47,3 +47,30 @@ function getMenuButtonFooter(message) {
 	return;
 }
 
+// 参考： https://syncer.jp/javascript-reverse-reference/output-local-image
+// [#form]が変更された時にイベントを実行する
+//document.getElementById( "picture" ).onchange = function()
+function encodeBase64(file) 
+{
+	var fileList , file , fr , result ;
+	fileList = this.files ;
+	for( var i=0,l=fileList.length; l>i; i++ )
+	{
+		file = fileList[i] ;
+		fr = new FileReader() ;
+		fr.onload = function()
+		{
+			return this.result
+            $.ajax({
+                type: 'POST',
+                url: '../api/picture',
+                contentType:'image/jepg',
+                data: {src: this.result},
+                success: function(xml) {
+                    $('#picture').text(xml.picture);
+                }
+            });
+		}
+        fr.readAsDataURL(file);
+	}
+}
